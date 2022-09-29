f8909087b5dc81a67433a0fa32e578e6ed308b80
Scott Motte <mot@mot.la>
2022-09-29 12:20:21 -0700

    Add dotenv-vault

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,8 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
+require('dotenv-vault').config({ debug: true })
+
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
