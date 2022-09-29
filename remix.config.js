35709f85039e5a3c832872a680ce477c1d4dbdc7
Scott Motte <mot@mot.la>
2022-09-29 12:23:34 -0700

    Update config

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,8 @@
  */
 require('dotenv-vault').config({ debug: true })
 
+console.log(process.env) // output for debugging purposes. remove when going to production.
+
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
